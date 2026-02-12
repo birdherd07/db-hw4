e0310679df6a6f439849fe84462c3f62099b61ff
birdtummy <99448204+birdtummy@users.noreply.github.com>
2026-02-12 00:01:07 -0600

    Added ORM driver

---
+++ docInc/OrmDriver.py
@@ -0,0 +1,63 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docIncDb.settings')
+
+import django
+django.setup()
+
+from docIncApp.models import Person
+
+def create_people():
+    new_person = Person.objects.create(
+        personname = "Gary Smith",
+        birthday = 12122008,
+        lastfoursocial = 1111
+    )
+    new_person2 = Person.objects.create(
+        personname = "John Smith",
+        birthday = 12122008,
+        lastfoursocial = 1112
+    )
+
+    print(new_person.id)
+    print(new_person2.id)
+
+def read_people():
+    try:
+        Person.objects.get(id=2)
+    except Person.DoesNotExist:
+        print("Person with id 2 was not found")
+    
+    people = Person.objects.all()
+    for person in people:
+        print(person)
+
+def update_people():
+    people_named_Gary = Person.objects.filter(personname__icontains="Gary")
+    people_named_Gary.update(birthday = 12121998)
+
+def delete_person():
+    person = Person.objects.get(id=2)
+    person.delete()
+
+    try:
+        Person.objects.get(id=2)
+    except Person.DoesNotExist:
+        print("Person with id 2 was deleted")
+
+def clear_table():
+    Person.objects.all().delete()
+
+if __name__ == '__main__':
+    print("Clearing table.")
+    clear_table()
+
+    print("Creating 2 entries.")
+    create_people()
+    input("Press enter to continue.\n>")
+
+    print("Updating 1 entry.")
+    read_people()
+    input("Press enter to continue.\n>")
+
+    print("Deleting 1 entry.")
+    delete_person()
